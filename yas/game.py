cd9e5dd93d5bebde13a661cbd8cb6750e169c3d0
Yasmin Miranda <ym220609@gmail.com>
2026-08-05 14:17:07 -0300

    Add files via upload

---
+++ yas/game.py
@@ -0,0 +1,16 @@
+'''
+* Yasmin Miranda
+* Aula 3 - Movimentação da Nave
+'''
+# Import
+import turtle
+
+# Janela
+janela = turtle.Screen()
+janela.title("Gamezinho")
+janela.bgcolor("pink")
+janela.setup(width=800, height=600)
+
+
+
+janela.mainloop()
